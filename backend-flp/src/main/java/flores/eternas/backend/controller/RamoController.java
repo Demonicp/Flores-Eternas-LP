67ccc9bd5aaba3f8266072309466987f680ad278
KappaXVI <montenegro.santiago@correounivalle.edu.co>
2026-06-08 16:04:15 -0500

    Rutas Protegidas

--- backend-flp/src/main/java/flores/eternas/backend/controller/RamoController.java
+++ backend-flp/src/main/java/flores/eternas/backend/controller/RamoController.java
@@ -2,6 +2,7 @@ package flores.eternas.backend.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -42,6 +43,7 @@ public class RamoController {
 
     //Santiago Montenegro HU6
     @PostMapping
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<RamoResponseDTO> crear(@RequestBody RamoRequestDTO dto) {
         RamoResponseDTO response = ramoService.crear(dto);
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
@@ -49,12 +51,14 @@ public class RamoController {
 
     //Santiago Montenegro HU6
     @PutMapping("/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<RamoResponseDTO> actualizar(@PathVariable Long id, @RequestBody RamoRequestDTO dto) {
         return ResponseEntity.ok(ramoService.actualizar(id, dto));
     }
 
     //Santiago Montenegro HU6
     @DeleteMapping("/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<Void> eliminar(@PathVariable Long id) {
         ramoService.eliminar(id);
         return ResponseEntity.noContent().build();
