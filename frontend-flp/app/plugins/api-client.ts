8758433e8fed7366775625970f83cbc41410f5d8
Demonicp <99213965+Demonicp@users.noreply.github.com>
2026-06-13 13:55:36 -0500

    Update api-client.ts

--- frontend-flp/app/plugins/api-client.ts
+++ frontend-flp/app/plugins/api-client.ts
@@ -1,5 +1,5 @@
 export default defineNuxtPlugin(() => {
-  const apiBase = process.env.NUXT_PUBLIC_API_BASE
+  const apiBase = process.env.NUXT_PUBLIC_API_BASE || 'http://localhost:8080'
   console.log("API Base configurado en:", apiBase)
   return {
     provide: {
