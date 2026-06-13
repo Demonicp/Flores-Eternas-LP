e9b1de03d10285994393e5b0f1c5065dd08c25a1
Demonicp <99213965+Demonicp@users.noreply.github.com>
2026-06-13 14:58:48 -0500

    Update api-client.ts

--- frontend-flp/app/plugins/api-client.ts
+++ frontend-flp/app/plugins/api-client.ts
@@ -1,17 +1,5 @@
-import { defineNuxtPlugin } from '#app'
-import { setApiBase } from '~/services/api-client'
-
 export default defineNuxtPlugin(() => {
-  let apiBase
-
-  if (import.meta.env.DEV) {
-    // 👉 Cuando corres npm run dev (local)
-    apiBase = 'http://localhost:8080'
-  } else {
-    // 👉 Cuando despliegas en Vercel (producción)
-    apiBase = 'https://flores-eternas-lp.onrender.com'
-  }
-
+  const apiBase = 'https://flores-eternas-lp.onrender.com'
   setApiBase(apiBase)
   console.log('API Base configurado en:', apiBase)
 })
