788a8338381edf29c952f48084a2cd372a0f3ba8
Demonicp <99213965+Demonicp@users.noreply.github.com>
2026-06-13 14:40:54 -0500

    Update api-client.ts

--- frontend-flp/app/plugins/api-client.ts
+++ frontend-flp/app/plugins/api-client.ts
@@ -1,5 +1,10 @@
+import { defineNuxtPlugin } from '#app'
+import { setApiBase } from '~/services/api-client'
+
 export default defineNuxtPlugin(() => {
+  // Usa la variable pública si existe
   const apiBase = import.meta.env.NUXT_PUBLIC_API_BASE_URL || 'http://localhost:8080'
-  console.log("API Base configurado en:", apiBase)
-  return { provide: { apiBase } }
+  setApiBase(apiBase)
+
+  console.log('API Base configurado en:', apiBase)
 })
